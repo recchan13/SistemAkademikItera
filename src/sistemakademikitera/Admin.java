cd46373fe7c9f1e795fad924e2d8bafe438b04f1
recchan13 <93rezao@gmail.com>
2019-05-17 12:02:04 +0700

    memperbaiki data yang hilang :")

--- src/sistemakademikitera/Admin.java
+++ src/sistemakademikitera/Admin.java
@@ -86,8 +86,9 @@ public class Admin extends Abs {
     }
     
     public void tambah(Connection con)throws SQLException{
+        
         Statement stmt = con.createStatement();
-        String query="INSERT INTO admin(username,password,tempat_peminjaman_id) VALUE ('" +username+ "','" +pass+ "','" +loc+ "')";
+        String query="INSERT INTO admin(username,password,tempat_peminjaman_id) VALUE ('" +username+ "','" +pass+ "'," +loc+ ")";
         
         if(stmt.executeUpdate(query)==1){  
         }
